d0ef219c82ecc5cf95ac6dd6ade111070fb0c931
Dorotea <doroteagrdan@gmail.com>
2022-12-30 14:02:55 +0100

    finalized app

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [dice, setDice] = useState(getNewDice());
   const [tenzies, setTenzies] = useState(false);
+  const [numbersOfRoll, setNumbersOfRoll] = useState([])
+  const [currentRollNumber, setCurrentRollNumber] = useState(1)
+  const [bestScore, setBestScore] = useState(0)
 
-  useEffect(()=>{
-      const allEqualNumbers = arr => arr.every(val => val === arr[0]);
-      const resultNum = allEqualNumbers(dice.map(die=>die.value))
-      const allHeld = arr => arr.every(val => val === arr[0]);
-      const isAllHeld = allHeld(dice.map(die=>die.isHeld))
-      if(resultNum && isAllHeld){
-        setTenzies(true)
-      }
-    },[dice])
-  
+  useEffect(() => {
+    const allEqualNumbers = arr => arr.every(val => val === arr[0]);
+    const resultNum = allEqualNumbers(dice.map(die => die.value))
+    const allHeld = arr => arr.every(val => val === arr[0]);
+    const isAllHeld = allHeld(dice.map(die => die.isHeld))
+    if (resultNum && isAllHeld) {
+      setTenzies(true);
+      setNumbersOfRoll(prevNumberOfRolls => [
+        ...prevNumberOfRolls,
+        currentRollNumber
+      ]);
+    }
+  }, [dice])
+
+  useEffect(() => {
+    setBestScore(Math.min(...numbersOfRoll));
+  }, [numbersOfRoll])
+
   function getRandomNum() {
     const randomNum = Math.floor(Math.random() * 6) + 1;
     return randomNum;
@@ -42,14 +53,16 @@ function App() {
   }
 
   function rollAgain() {
-    setDice(prevDice=>{
-      return prevDice.map(prevDie=>{
-        if(!prevDie.isHeld){
+    setCurrentRollNumber(prevRollNum => prevRollNum + 1)
+    console.log(currentRollNumber)
+    setDice(prevDice => {
+      return prevDice.map(prevDie => {
+        if (!prevDie.isHeld) {
           return {
             ...prevDie,
             value: getRandomNum()
           }
-        } 
+        }
         return prevDie;
       })
     });
@@ -58,26 +71,30 @@ function App() {
   function setHeldDie(id) {
     setDice(prevDice => {
       return prevDice.map((die, index) => {
-          if (index === id) {
-            return {
-              ...die,
-              isHeld: !die.isHeld
-            }
+        if (index === id) {
+          return {
+            ...die,
+            isHeld: !die.isHeld
           }
-          return die
-        })
+        }
+        return die
+      })
     })
   }
 
-  const gameText = 
-  <><h2>Tenzies</h2>
-  <p>Roll a dice until you have all the same numbers.</p>
-  <p>To save the dice click on it!</p></>;
-  const gameWon = <>
-  <h2>You won!</h2>
-  <p>You rolled the same number.</p>
-  <p>Congrats!</p>
-  </>; 
+
+  const gameText =
+    <>
+      <h2>Tenzies</h2>
+      <p>Roll a dice until you have all the same numbers.</p>
+      <p>To save the dice click on it!</p>
+    </>;
+  const gameWon =
+    <>
+      <h2>You won!</h2>
+      <p>It took you {currentRollNumber} rolls.</p>
+      <p>Your best score is {bestScore}!</p>
+    </>;
 
   return (
     <div className="App">
@@ -85,7 +102,13 @@ function App() {
       <div id="dice">
         {displayDie()}
       </div>
-      <button id="rollBtn" onClick={tenzies ? ()=>{setDice(getNewDice); setTenzies(false);} : rollAgain}>{tenzies ? "Next game" : "Roll Again"}</button>
+      <button id="rollBtn" onClick={tenzies ? () => {
+          setDice(getNewDice);
+          setTenzies(false);
+          setCurrentRollNumber(0)
+        } : rollAgain}>
+        {tenzies ? "Next game" : "Roll Again"}
+      </button>
     </div>
   );
 }
